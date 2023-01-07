b2a3589df521970b3a2ebd6eeb0dec53eeb3dbb5
Prashant <prashant.chaurasiya.cp@gmail.com>
2023-01-07 12:42:57 +0530

    [Prashant]: CSV file reader

---
+++ ThirdPartLibrary/ThirdPartLibrary/Address.cs
@@ -0,0 +1,43 @@
+﻿using CsvHelper;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirdPartLibrary
+{
+    public class Address
+    {
+        public static string AddressFilePath = @"E:\\New folder\\ThirdPartLibrary\\ThirdPartLibrary\\Utility\\Address.csv";
+        public static string ImportFilePath = @"E:\\New folder\\ThirdPartLibrary\\ThirdPartLibrary\\Utility\\Import.csv";
+        public static void ThirdParty()
+        {
+            using(var reader = new StreamReader(AddressFilePath))
+            using(var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<AddressModel>().ToList();
+                Console.WriteLine("Read data from csvFile");
+                foreach(AddressModel address in records)
+                {
+                    Console.WriteLine("\n" + address.FirstName);
+                    Console.WriteLine("\n" + address.LastName);
+                    Console.WriteLine("\n" + address.MobileNumber);
+                    Console.WriteLine("\n" + address.Address);
+                }
+                using(var writer = new StreamWriter(ImportFilePath)) 
+                    using(var csvImport = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                {
+                    csvImport.WriteRecords(records);
+                }
+
+
+            }
+
+            
+        }
+            
+    }
+}
---
+++ ThirdPartLibrary/ThirdPartLibrary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ThirdPartLibrary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Address.ThirdParty();
+
+        }
+    }
+}
